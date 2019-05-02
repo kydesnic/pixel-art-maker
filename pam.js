c08dc4855fee871d09c3e9bf1ca6751eb737d41f
Alexey Artemiev <akydesnic@mail.ru>
2019-05-03 00:04:21 +0300

    Create pixel array for canvas.

--- pam.js
+++ pam.js
@@ -1,2 +1,29 @@
-let c = document.getElementById("canvas");
-c.innerHTML = "HELLO!";
+function Canvas(width, height) {
+    const canvas = { width: width, height: height };
+
+    canvas.element = document.getElementById('canvas');
+    canvas.element.innerHTML = '';
+    canvas.element.style.backgroundColor = '#e5e5e5';
+
+    const pixelSize = '8px';
+
+    for (let y = 0; y < height; ++y) {
+        const row = document.createElement('div');
+        row.style.lineHeight = '0';
+
+        for (let x = 0; x < width; ++x) {
+            const pixel = document.createElement('div');
+            pixel.style.width = pixelSize;
+            pixel.style.height = pixelSize;
+            pixel.style.backgroundColor = '#ffffff';
+            pixel.style.display = 'inline-block';
+            pixel.style.border = '1px solid';
+            pixel.style.borderColor = canvas.element.style.backgroundColor;
+
+            row.appendChild(pixel);
+        }
+        canvas.element.appendChild(row);
+    }
+}
+
+let canvas = Canvas(4, 4);
